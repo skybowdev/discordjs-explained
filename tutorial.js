a69f2fccdec67ee751d1aca4fe4017e6a637c9a8
thunderstorm010 <66248615+thunderstorm010@users.noreply.github.com>
2021-04-03 15:28:20 +0300

    Improve tutorial

--- tutorial.js
+++ tutorial.js
@@ -5,19 +5,19 @@ client.on('ready', () => { //function to be ran once our bot is ready
   console.log(`Logged in as ${client.user.tag}!`); //console log's the bot's name with its tag, eg: skybow#9999
 });
 
-client.on('message', msg => { //so we are async'ing msg so our code nows that msg is the message of our user
-  if (msg.content === 'ping') { //checks the message sent to see if it says ping
-    msg.reply('Pong!'); //replies with Pong! upon seeing that our text includes ping
+client.on('message', msg => { // Listening for a message
+  if (msg.content === 'ping') { // checks the content of the message, if it matches 'ping'
+    msg.reply('Pong!'); // Replies the message with 'Pong!'. Note: this doesn't make an inline reply.
   }
 });
 
-const embed = new Discord.MessageEmbed() //we created an embed here, now we need to fill it up
-.setTitle('') //adds a title to the embed, make sure its a string
-.setDescription('') //description of the embed
-.setColor('') //use color codes such as #123ad or you can use regular colours like, RED or BLUE
+const embed = new Discord.MessageEmbed() // we initialized an empty embed object
+.setTitle('Example Title') //adds a title to the embed, make sure its a string
+.setDescription('Example Description') //description of the embed
+.setColor('RED') //use color codes such as #123ad or you can use regular colours like, RED or BLUE
 .addField('field\'s title', 'the things inside the field') //fields are interesting... //pretty sure markdown is enabled doe
-.addField('', '') //yes you can have multiple fields....
-client.login('token'); //logs in to the bot
+.addField('another field', 'fields value') //yes you can have multiple fields....
+client.login('token'); //logs in to the bot. warning: don't hardcode the token
 
 //fun fact: a \ is an escape chracter, you can use \' to add a ' smybol without closing the string.
 //theres more to discord.js, this is just the basics explained briefly
